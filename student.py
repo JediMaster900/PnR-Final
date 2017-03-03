4ed6fc7148227f4482da02ab2ef22295038f6929
jedimaster900 <charliemendes@msn.com>
2017-03-03 10:40:06 -0500

    Adding to scanning function

--- student.py
+++ student.py
@@ -61,6 +61,37 @@ class GoPiggy(pigo.Pigo):
         # activate the item selected
         menu.get(ans, [None, error])[1]()
 
+    def count_obstacles(self):
+        # run a scan
+        self.wide_scan()
+        # count how many obstacles I've found
+        counter = 0
+        # starting state assumes no obstacle
+        found_something = False
+        # loop through all my scan data
+        for x in self.scan:
+            # if x is not None and really close
+            if x and x < self.STOP_DIST:
+                # if I've already found something
+                if found_something:
+                    print("obstacle continues")
+                    # if this is a new obstacle
+                else:
+                    # switch my tracker
+                    found_something = True
+                    print("start of new obstacle")
+            # if my sata show safe distances...
+            if x and x > self.STOP_DIST:
+                # if my tracker had been triggered...
+                if found_something:
+                    print("end of obstacle")
+                    # reset tracker
+                    found_something = False
+                    # increase count of obstacles
+                    counter += 1
+        print('Total number of obstacles in this scan: ' + str(counter))
+        return counter
+
     def look_fwd(self):
         print('Looking forward...')
         self.servo(self.MIDPOINT)
