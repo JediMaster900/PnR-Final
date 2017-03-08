cc48289068292d5a27d782b158de4c52af170624
jedimaster900 <charliemendes@msn.com>
2017-03-08 10:56:37 -0500

    Creating self navigation

--- student.py
+++ student.py
@@ -195,14 +195,14 @@ class GoPiggy(pigo.Pigo):
         print("[ Press CTRL + C to stop me, then run stop.py ]\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         # this is the loop part of the "main logic loop"
-    while True:
-        if self.is_clear():
-            self.cruise()
-        answer = self.choose_path()
-        if answer == "left":
-            self.encL(6)
-        elif answer == "right":
-            self.encR(6)
+        while True:
+            if self.is_clear():
+                self.cruise()
+            answer = self.choose_path()
+            if answer == "left":
+                self.encL(6)
+            elif answer == "right":
+                self.encR(6)
     ############################
     ### NAV END ################
     ############################
